f6a523a5b26ca47659dad785a077e33ca98144cb
Gabriel Zapodeanu <gzapodea@cisco.com>
2017-05-29 21:36:09 -0700

    Create DEVNET_2593_init.py
    This is the Init file for the MerakiConnect App

---
+++ DEVNET_2593_init.py
@@ -0,0 +1,27 @@
+
+# developed by Gabi Zapodeanu, TSA, GSS, Cisco Systems
+
+# !/usr/bin/env python3
+
+# Spark init info
+
+SPARK_URL = 'https://api.ciscospark.com/v1'
+SPARK_AUTH = 'Bearer ' + 'Place your Spark Token here'
+SPARK_ROOM = 'DEVNET_2593_lab'
+SPARK_TEAM = 'DEVNET_2593'
+SPARK_EMAIL = 'email to add memebership'
+
+
+# Meraki init info
+
+MERAKI_API_KEY = 'Place your Meraki API key here' 
+MERAKI_CUSTOMER_NUMBER = 'Place your Meraki customer number here' 
+MERAKI_URL = 'https://dashboard.meraki.com/api/v0'
+MERAKI_ORG = 'Meraki Organization name'
+MERAKI_NETWORK = 'APIsDemo'
+MERAKI_DEVICE_SN = 'Meraki device SN'
+MERAKI_CLIENT_MAC = '78:a3:e4:f1:de:f2'
+MERAKI_PHONE_NO = 'Meraki SM Client phone number'
+MERAKI_GUEST_SSID = 'MerakiConnect'
+
+
